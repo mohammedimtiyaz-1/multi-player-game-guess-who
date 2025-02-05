80258c0e256745d3c2e22ae2e157a26803db2a5d
imtiyaz <mohammedimtiyaz.jk@yahoo.com>
2025-02-05 22:15:54 +0530

    relative path fixed

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/guess-who/", // Update this to match your repository name
+  base: "./", // Update this to match your repository name
   build: {
     outDir: "dist",
     assetsDir: "assets",
